f2b52d346bf74860610a9a95a68a8a2728d20ff4
Richard <richard.l.huynh@intel.com>
2016-07-13 15:31:53 -0500

    initial commit

---
+++ article_maker/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+# Create your models here.
+# Specfications on how the database should be built
---
+++ article_maker/tests.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+# Create your tests here.
+"""
+	test case: see if the name contains numbers
+	test case: numbers for letters
+	test case: not real name
+	test case: blank
+	test case: punctuation
+"""
+
